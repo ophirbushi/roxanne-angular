ce45dbe559259538b39cd8937bbea4203bf09bb9
Ophir Bushinsky <ophirbushi@gmail.com>
2019-01-02 17:20:33 +0200

    provide store in angular-roxanne module forRoot

--- projects/roxanne-angular/src/lib/roxanne-angular.module.ts
+++ projects/roxanne-angular/src/lib/roxanne-angular.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RoxanneAngularComponent } from './roxanne-angular.component';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 
-@NgModule({
-  declarations: [RoxanneAngularComponent],
-  imports: [
-  ],
-  exports: [RoxanneAngularComponent]
-})
-export class RoxanneAngularModule { }
+import { Reducer, Effects, Store } from 'roxanne';
+
+@NgModule({})
+export class RoxanneAngularModule {
+
+  static forRoot<State, Actions>(initialState: State, reducer: Reducer<State, Actions>,
+    effects?: Effects<State, Actions>[]): ModuleWithProviders {
+    return {
+      ngModule: RoxanneAngularModule,
+      providers: [{ provide: Store, useValue: new Store<State, Actions>(initialState, reducer, effects) }]
+    };
+  }
+
+}
--- projects/roxanne-angular/src/public_api.ts
+++ projects/roxanne-angular/src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of roxanne-angular
  */
 
-export * from './lib/roxanne-angular.service';
-export * from './lib/roxanne-angular.component';
 export * from './lib/roxanne-angular.module';
